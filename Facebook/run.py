4bc4e045e9fda342c183cb5b6b3cc3efad656d74
Asım <asimzorlu@gmail.com>
2022-05-25 10:58:29 +0300

    Update run.py

--- Facebook/run.py
+++ Facebook/run.py
@@ -1,114 +1,113 @@
 import os
 
-
 def saruhanli():
-    os.system('D:')
-    os.chdir(r'D:\\Projects\\Sosyal\\Facebook\\Saruhanli')
+    os.system('C:')
+    os.chdir(r'C:\\Projects\\Sosyal\\Facebook\\Saruhanli')
     os.system('python saruhanli.py')
 
 def ayrancilar():
-    os.system('D:')
-    os.chdir(r'D:\\Projects\\Sosyal\\Facebook\\Ayrancilar')
+    os.system('C:')
+    os.chdir(r'C:\\Projects\\Sosyal\\Facebook\\Ayrancilar')
     os.system('python ayrancilar.py')
 
 def esme():
-    os.system('D:')
-    os.chdir(r'D:\\Projects\\Sosyal\\Facebook\\Esme')
+    os.system('C:')
+    os.chdir(r'C:\\Projects\\Sosyal\\Facebook\\Esme')
     os.system('python esme.py')
 
 def tavsanli():
-    os.system('D:')
-    os.chdir(r'D:\\Projects\\Sosyal\\Facebook\\Tavsanli')
+    os.system('C:')
+    os.chdir(r'C:\\Projects\\Sosyal\\Facebook\\Tavsanli')
     os.system('python tavsanli.py')
 
 def nazilli():
-    os.system('D:')
-    os.chdir(r'D:\\Projects\\Sosyal\\Facebook\\Nazilli')
+    os.system('C:')
+    os.chdir(r'C:\\Projects\\Sosyal\\Facebook\\Nazilli')
     os.system('python nazilli.py')
 
 def manavgat():
-    os.system('D:')
-    os.chdir(r'D:\\Projects\\Sosyal\\Facebook\\Manavgat')
+    os.system('C:')
+    os.chdir(r'C:\\Projects\\Sosyal\\Facebook\\Manavgat')
     os.system('python manavgat.py')
 
 def koycegiz():
-    os.system('D:')
-    os.chdir(r'D:\\Projects\\Sosyal\\Facebook\\Koycegiz')
+    os.system('C:')
+    os.chdir(r'C:\\Projects\\Sosyal\\Facebook\\Koycegiz')
     os.system('python koycegiz.py')
 
 def kilkenny():
-    os.system('D:')
-    os.chdir(r'D:\\Projects\\Sosyal\\Facebook\\Kilkenny')
+    os.system('C:')
+    os.chdir(r'C:\\Projects\\Sosyal\\Facebook\\Kilkenny')
     os.system('python kilkenny.py')
 
 def guney():
-    os.system('D:')
-    os.chdir(r'D:\\Projects\\Sosyal\\Facebook\\Guney')
+    os.system('C:')
+    os.chdir(r'C:\\Projects\\Sosyal\\Facebook\\Guney')
     os.system('python guney.py')
 
 def evcimen():
-    os.system('D:')
-    os.chdir(r'D:\\Projects\\Sosyal\\Facebook\\Evcimen')
+    os.system('C:')
+    os.chdir(r'C:\\Projects\\Sosyal\\Facebook\\Evcimen')
     os.system('python evcimen.py')
 
 def izmir():
-    os.system('D:')
-    os.chdir(r'D:\\Projects\\Sosyal\\Facebook\\Izmir')
+    os.system('C:')
+    os.chdir(r'C:\\Projects\\Sosyal\\Facebook\\Izmir')
     os.system('python izmir.py')
 
 def soke():
-    os.system('D:')
-    os.chdir(r'D:\\Projects\\Sosyal\\Facebook\\Soke')
+    os.system('C:')
+    os.chdir(r'C:\\Projects\\Sosyal\\Facebook\\Soke')
     os.system('python soke.py')
 
 def torbali():
-    os.system('D:')
-    os.chdir(r'D:\\Projects\\Sosyal\\Facebook\\Torbali')
+    os.system('C:')
+    os.chdir(r'C:\\Projects\\Sosyal\\Facebook\\Torbali')
     os.system('python torbali.py')
 
 def oyun_en():
-    os.system('D:')
-    os.chdir(r'D:\\Projects\\Sosyal\\Facebook\\Oyun')
+    os.system('C:')
+    os.chdir(r'C:\\Projects\\Sosyal\\Facebook\\Oyun')
     os.system('python oyun_en.py')
 
 def oyun_tr():
-    os.system('D:')
-    os.chdir(r'D:\\Projects\\Sosyal\\Facebook\\Oyun')
+    os.system('C:')
+    os.chdir(r'C:\\Projects\\Sosyal\\Facebook\\Oyun')
     os.system('python oyun_tr.py')
 
 def kahta():
-    os.system('D:')
-    os.chdir(r'D:\\Projects\\Sosyal\\Facebook\\Kahta')
+    os.system('C:')
+    os.chdir(r'C:\\Projects\\Sosyal\\Facebook\\Kahta')
     os.system('python kahta.py')
 
 def midyat():
-    os.system('D:')
-    os.chdir(r'D:\\Projects\\Sosyal\\Facebook\\Midyat')
+    os.system('C:')
+    os.chdir(r'C:\\Projects\\Sosyal\\Facebook\\Midyat')
     os.system('python midyat.py')
 
 def ege():
-    os.system('D:')
-    os.chdir(r'D:\\Projects\\Sosyal\\Facebook\\Ege')
+    os.system('C:')
+    os.chdir(r'C:\\Projects\\Sosyal\\Facebook\\Ege')
     os.system('python egesozluk.py')
 
 def alasehir():
-    os.system('D:')
-    os.chdir(r'D:\\Projects\\Sosyal\\Facebook\\Alaşehir')
+    os.system('C:')
+    os.chdir(r'C:\\Projects\\Sosyal\\Facebook\\Alaşehir')
     os.system('python alasehir.py')
 
 def halisaha():
-    os.system('D:')
-    os.chdir(r'D:\\Projects\\Sosyal\\Facebook\\HalıSaha')
+    os.system('C:')
+    os.chdir(r'C:\\Projects\\Sosyal\\Facebook\\Halısaha')
     os.system('python halisaha.py')
 
 def goztepe():
-    os.system('D:')
-    os.chdir(r'D:\\Projects\\Sosyal\\Facebook\\Göztepe')
+    os.system('C:')
+    os.chdir(r'C:\\Projects\\Sosyal\\Facebook\\Göztepe')
     os.system('python gozgoz.py')
 
 def pet():
-    os.system('D:')
-    os.chdir(r'D:\\Projects\\Sosyal\\Facebook\\PetSahiplen')
+    os.system('C:')
+    os.chdir(r'C:\\Projects\\Sosyal\\Facebook\\PetSahiplen')
     os.system('python pet_sahiplen.py')
 
 deger = input("Hangi app?\n")
