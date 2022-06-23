84d5b4ef2412ad7d9ddbdae66f88c46a2553182b
Asım <asimzorlu@gmail.com>
2022-06-23 10:02:43 +0200

    Add files via upload
    Accepting of cookies was committed.

--- Facebook/init.py
+++ Facebook/init.py
@@ -2,13 +2,15 @@ from selenium import webdriver
 from webdriver_manager.chrome import ChromeDriverManager
 import time
 import warnings
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.action_chains import ActionChains
 
 class Login:
     def login(self):
         warnings.filterwarnings("ignore")
         chrome_options = webdriver.ChromeOptions()
         chrome_options.add_argument('--window-size=1036, 674')
-        chrome_options.add_argument('--headless')
+        #chrome_options.add_argument('--headless')
         chrome_options.add_argument("--log-level=3")
         chrome_options.add_experimental_option("prefs", {
             "profile.default_content_setting_values.notifications": 1
@@ -17,10 +19,24 @@ class Login:
         self.browser = webdriver.Chrome(executable_path=ChromeDriverManager().install(), options=chrome_options, )
         self.browser.get('https://www.facebook.com/')
 
+        time.sleep(5)
+
+        N = 26  # number of times you want to press TAB
+
+        actions = ActionChains(self.browser)
+        for _ in range(N):
+            actions = actions.send_keys(Keys.TAB)
+        actions.perform()
+
+        time.sleep(2)
+        actions1 = ActionChains(self.browser)
+        actions1.send_keys(Keys.RETURN)
+        actions1.perform()
+
         time.sleep(2)
         email = self.browser.find_element_by_id('email')
-        email.send_keys("Your Email Address")
+        email.send_keys("nagavaga")
 
         time.sleep(2)
         password = self.browser.find_element_by_id('pass')
-        password.send_keys("Your Password")
+        password.send_keys("haschmeth250*7")
