5cc9fa053df092c4abae5129854629c32dff6e90
Asım <asimzorlu@gmail.com>
2022-07-27 18:10:58 +0200

    Update init.py

--- Facebook/init.py
+++ Facebook/init.py
@@ -10,7 +10,7 @@ class Login:
         warnings.filterwarnings("ignore")
         chrome_options = webdriver.ChromeOptions()
         chrome_options.add_argument('--window-size=1036, 674')
-        #chrome_options.add_argument('--headless')
+        chrome_options.add_argument('--headless')
         chrome_options.add_argument("--log-level=3")
         chrome_options.add_experimental_option("prefs", {
             "profile.default_content_setting_values.notifications": 1
