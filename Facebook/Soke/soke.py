e2802d3f2d6be4c42c06cf7cb23aa6279f97b6c3
Asım <asimzorlu@gmail.com>
2023-01-03 23:28:06 +0100

    Update soke.py

--- Facebook/Soke/soke.py
+++ Facebook/Soke/soke.py
@@ -47,9 +47,7 @@ soke = ["718233511622533", "2198720647021293", "2614247848875259", "183032711887
         "1724842091109410", "447280948738495", "1598433740486562", "1652649881642707", "113022235388425",
         "1083834084969974", "107121203140839"]
 
-list = sample(soke, len(soke))
-
-for i in range(len(soke)):
-    group_url = 'https://www.facebook.com/groups/' + list[i] + '/buy_sell_discussion'
-    fb.post_on_facebook(group_url)
+list = random.choice(soke)
+group_url = 'https://www.facebook.com/groups/' + list + '/buy_sell_discussion'
+fb.post_on_facebook(group_url)
 fb.close_browser()
