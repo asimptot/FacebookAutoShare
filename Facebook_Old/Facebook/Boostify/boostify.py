f8c709dab2ed45cf4d4dceba24fb7dae91c4cd20
Asım <asimzorlu@gmail.com>
2023-04-12 10:35:22 +0300

    Update boostify.py

--- Facebook_Old/Facebook/Boostify/boostify.py
+++ Facebook_Old/Facebook/Boostify/boostify.py
@@ -1,22 +1,17 @@
-import time
 import sys
 sys.path.append(r'C:\\Projects\\Facebook_Old\\Facebook')
 from init import *
-from selenium.webdriver.common.by import By
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
-from selenium.webdriver.common.action_chains import ActionChains
 
 class FBPost:
     def setup(self):
         Setup.login(self)
-        time.sleep(2)
+        sleep(2)
 
     def post_on_facebook(self, group_url):
         print('Posting  on Facebook group: ', group_url)
-        time.sleep(4)
+        sleep(4)
         self.browser.get(group_url)
-        time.sleep(2)
+        sleep(2)
 
         text_to_post = 'Dear friends,\n\n' \
                        'Boostify app is now on our mobile phone. ' \
@@ -36,18 +31,18 @@ class FBPost:
             post_class = post_class.replace(' ', '.')
             click_post = self.browser.find_element(By.CLASS_NAME, post_class)
             click_post.click()
-            time.sleep(10)
+            sleep(10)
 
             actions = ActionChains(self.browser)
             actions.send_keys(text_to_post).perform()
-            time.sleep(5)
+            sleep(5)
 
             send_post = WebDriverWait(self.browser, 10).until(
                 EC.element_to_be_clickable((By.XPATH,
                                             '//*[starts-with(@id,"mount_0_0")]/div/div[1]/div/div[4]/div/div/div[1]/div/div[2]/div/div/div/div/div[1]/form/div/div[1]/div/div/div[1]/div/div[3]/div[2]/div/div/div'))
             )
             send_post.click()
-            time.sleep(300)
+            sleep(300)
         except:
             print("Something went wrong, exiting script to avoid conflicts")
 
@@ -57,7 +52,7 @@ class FBPost:
 fb = FBPost()
 fb.setup()
 
-boostify = ["305244213921743", "managers.social.media", "2815428158737599", "boostsocialmedia.net",
+boostify = ["568620580948603", "managers.social.media", "2815428158737599", "boostsocialmedia.net",
             "talentedindividual", "socialmediamarketinghubsmmh", "233921228105287", "4071283406227548",
             "socialmediamarketinggroup", "458611971395741", "677369376241448", "343581006189180",
             "523395511877590", "593847758000515", "128473199098432", "299735715633163", "1861331500694686",
@@ -66,12 +61,13 @@ boostify = ["305244213921743", "managers.social.media", "2815428158737599", "boo
             "1564518510516445", "instagramfollowersgo", "3220778951364530", "OpenGroupFree", "5586200261456329",
             "103096463458577", "openaicontent", "1413062589012533", "515574005256991", "264691513711120",
             "498094584732613", "347316160599691", "2844512592450510", "557003561120252", "897525264308501",
-            "youtubesubscribersfan", "177620938358820", "280724023017288", "177994190590941", "568620580948603"]
+            "youtubesubscribersfan", "177620938358820", "280724023017288", "177994190590941", "305244213921743"]
 
+list = sample(boostify, len(boostify))
 for i in range(len(boostify)):
-    group_url = 'https://www.facebook.com/groups/' + boostify[i] + '/buy_sell_discussion'
+    group_url = 'https://www.facebook.com/groups/' + list[i] + '/buy_sell_discussion'
     fb.post_on_facebook(group_url)
     if i % 5 == 0:
         print('1 saat bekletiliyor...')
-        time.sleep(3600)
+        sleep(3600)
 fb.close_browser()
